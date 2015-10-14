8a3fcaad7201c9e63cce47a2722f0b025bb757b1
Eric Suarez <egs104@gmail.com>
2015-10-14 11:53:41 -0400

    first commit

---
+++ ParseStarterProject-Swift/ParseStarterProject/ViewController.swift
@@ -0,0 +1,74 @@
+/**
+* Copyright (c) 2015-present, Parse, LLC.
+* All rights reserved.
+*
+* This source code is licensed under the BSD-style license found in the
+* LICENSE file in the root directory of this source tree. An additional grant
+* of patent rights can be found in the PATENTS file in the same directory.
+*/
+
+import UIKit
+import Parse
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var username: UITextField!
+    @IBOutlet weak var password: UITextField!
+    
+    
+    var activityIndicator: UIActivityIndicatorView = UIActivityIndicatorView()
+    
+    func displayAlert(title: String, message: String) {
+        
+        var alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertControllerStyle.Alert)
+        alert.addAction((UIAlertAction(title: "OK", style: .Default, handler: { (action) -> Void in
+            self.dismissViewControllerAnimated(true, completion: nil)
+        })))
+        
+        self.presentViewController(alert, animated: true, completion: nil)
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    @IBAction func login(sender: AnyObject) {
+        
+        var errorMessage = "Please try again later"
+        
+        PFUser.logInWithUsernameInBackground(username.text!, password: password.text!, block: { (user, error) -> Void in
+        
+                self.activityIndicator.stopAnimating()
+                UIApplication.sharedApplication().endIgnoringInteractionEvents()
+        
+                if user != nil {
+        
+                    print("login successful!")
+        
+                } else {
+        
+                    if let errorString = error!.userInfo["error"] as? String {
+        
+                        errorMessage = errorString
+        
+                    }
+        
+                    self.displayAlert("Login Failed", message: errorMessage)
+                }
+                    
+        })
+        
+        
+    }
+
+}
